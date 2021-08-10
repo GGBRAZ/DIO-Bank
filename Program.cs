f0ead2acc0220db0e04cbf27be0f9ecb9e062b57
Guilherme Gomes do Braz <ggbraz@gmail.com>
2021-08-09 23:19:40 -0300

    implementando menu da aplicação

--- Program.cs
+++ Program.cs
@@ -2,11 +2,56 @@
 
 namespace DIO_Bank
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+      string opcaoUsuario = ObterOpcaoUsuario();
+      while (opcaoUsuario.ToUpper() != "X")
+      {
+        switch (opcaoUsuario)
         {
-            Console.WriteLine("Hello World!");
+          case "1":
+            // ListarContas();
+            break;
+          case "2":
+            // InserirConta();
+            break;
+          case "3":
+            // Transferir();
+            break;
+          case "4":
+            // Sacar();
+            break;
+          case "5":
+            // Depositar();
+            break;
+          case "C":
+            Console.Clear();
+            break;
+          default:
+            throw new ArgumentOutOfRangeException();
         }
+      }
     }
+
+    private static string ObterOpcaoUsuario()
+    {
+      System.Console.WriteLine();
+      System.Console.WriteLine("DIO BANK a seu dispor!!!");
+      System.Console.WriteLine("Informe a opção desejada");
+      System.Console.WriteLine("1- Listar contas");
+      System.Console.WriteLine("2- Inserir nova conta");
+      System.Console.WriteLine("3- Transferir");
+      System.Console.WriteLine("4- Sacar");
+      System.Console.WriteLine("5- Depositar");
+      System.Console.WriteLine("C- Limpar a tela");
+      System.Console.WriteLine("X- Sair");
+      System.Console.WriteLine();
+
+      string opcaoUsuario = Console.ReadLine().ToUpper();
+      System.Console.WriteLine();
+      return opcaoUsuario;
+    }
+  }
 }
